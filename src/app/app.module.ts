c6e1a9463698171e8f34c0f12ab3d1185e7d06e3
Luis Saraza <luis.sarazag@gmail.com>
2020-03-30 03:18:43 -0700

    Layout
    - Created frontend architecture
    - Created Shell component
    - Created Home component - Containter
    - Created Header and Navbar component and implemented layout and styles
    - Created a mock server with a mock Api to feed the App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HomeModule} from '@app/home/home.module';
+import {ShellModule} from '@app/shell/shell.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ShellModule,
+    HomeModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
